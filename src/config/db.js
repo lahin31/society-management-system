aeaf5f0207813e1e32d2f8a1656df3b81917dbe6
Muhammad Lahin <muhammad.lahin@gmail.com>
2020-12-17 14:58:18 +0600

    env update

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 exports.makeDb = () => {
   mongoose.set("useCreateIndex", true);
   mongoose.connect(
-    `mongodb+srv://lahin:WMOhiutywbZLB1c0@cluster0.fzt7a.mongodb.net/society-management?retryWrites=true&w=majority`,
+    `mongodb+srv://${process.env.DATABASE_NAME}:${process.env.DATABASE_PASSWORD}@cluster0.fzt7a.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
